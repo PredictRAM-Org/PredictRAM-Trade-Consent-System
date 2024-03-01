03b7e34763d65691c73118bdcba82e0b3da1eda9
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2024-03-01 21:15:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ from urllib.parse import quote_plus
 app = Flask(__name__)
 
 # Flask-Mail configuration
-app.config['MAIL_SERVER'] = 'your_mail_server'
+app.config['MAIL_SERVER'] = 'support@predictram.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'your_username'
-app.config['MAIL_PASSWORD'] = 'your_password'
+app.config['MAIL_USERNAME'] = 'support'
+app.config['MAIL_PASSWORD'] = 'Singh@54812'
 
 mail = Mail(app)
 
