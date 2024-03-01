af4acab1555754119ae949eb7eb61dc0648c8a63
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2024-03-01 21:12:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ from flask_mail import Mail, Message
 from urllib.parse import quote_plus
 
 app = Flask(__name__)
+
+# Flask-Mail configuration
+app.config['MAIL_SERVER'] = 'your_mail_server'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'your_username'
+app.config['MAIL_PASSWORD'] = 'your_password'
+
 mail = Mail(app)
 
 # Generate a unique link
